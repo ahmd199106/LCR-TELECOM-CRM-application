c102978425af112730e73665854444ba4c4d1ed8
shabeeb <shabeebcoder@gmail.com>
2020-11-14 00:59:53 +0400

    router cleaning going on

--- src/App.js
+++ src/App.js
@@ -1,7 +1,4 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import MainNavBar from './pages/components/MainNavBar';
-import MainDrawer from './pages/components/MainDrawer';
 import Error from './pages/components/Error';
 // *** Import Application pages ***
 import Create_Purchase from './pages/purchase_create';
@@ -12,35 +9,19 @@ import Desktop_Login from './pages/desktop_login';
 import Price_Table from './pages/price_table';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: 'flex',
-    //fontSize: '6px',
-  },
-  content: {
-    flexGrow: '1',
-    padding: theme.spacing(3),
-    marginTop: theme.spacing(0),
-  },
-}));
-
 function App() {
-  const classes = useStyles();
-
   return (
-    <div className={classes.root}>
-      <MainNavBar />
-      <MainDrawer />
-      <main className={classes.content}>
-        <Switch>
-          <Route exact path="/" component={Purchase_dashboard} />
-          <Route exact path="/Purchase_List" component={Purchase_List} />
-          <Route exact path="/Create_Purchase" component={Create_Purchase} />
-          <Route exact path="/Purchase_Details" component={Purchase_Details} />
-          <Route component={Error} />
-        </Switch>
-      </main>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Purchase_dashboard} />
+        <Route exact path="/purchaselist" component={Purchase_List} />
+        <Route exact path="/createpurchase" component={Create_Purchase} />
+        <Route exact path="/purchasedetails" component={Purchase_Details} />
+        <Route exact path="/login" component={Desktop_Login} />
+        <Route exact path="/pricing" component={Price_Table} />
+        <Route component={Error} />
+      </Switch>
+    </Router>
   );
 }
 
